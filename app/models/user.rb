a4ca1dff06270203386e3ad7b8b36d5c93843050
inoue-souta <73748586+inoue-souta@users.noreply.github.com>
2020-12-24 22:04:02 +0900

    バリデーション記述　（htmlのフォームをコメントアウト）

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  with_options presence: true do
+    validates :name
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i}
+  end
+
+  has_many :camps
 end
