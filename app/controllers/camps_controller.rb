e8bb28b7093496322870ef8e8e020b04ed13f097
inoue-souta <73748586+inoue-souta@users.noreply.github.com>
2021-01-06 16:34:09 +0900

    投稿機能の基本機能実装（投稿一覧表示も実装）

--- app/controllers/camps_controller.rb
+++ app/controllers/camps_controller.rb
@@ -1,5 +1,25 @@
 class CampsController < ApplicationController
   def index
-    @camps = Camp.all
+    @camps = Camp.includes(:user)
   end
+
+  def new
+    @camp = Camp.new
+  end
+
+  def create
+    @camp = Camp.new(camp_params)
+    if @camp.save
+      redirect_to root_path
+    else 
+      render :new
+    end
+  end
+
+  private
+
+  def camp_params
+    params.require(:camp).permit(:title, :text, :category, :image).merge(user_id: current_user.id)
+  end
+
 end
--- app/models/camp.rb
+++ app/models/camp.rb
@@ -1,2 +1,11 @@
 class Camp < ApplicationRecord
+  has_one_attached :image
+  belongs_to :user
+
+  with_options presence: true do
+    validates :title
+    validates :category
+    validates :text
+    validates :image
+  end
 end
