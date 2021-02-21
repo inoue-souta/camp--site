f449a1875c49411c8909cb71bdcaf1229e56f8d5
inoue-souta <73748586+inoue-souta@users.noreply.github.com>
2021-02-21 15:40:05 +0900

    トップ画面のオススメ道具の詳細ページ遷移機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'camps#index'
   get 'explanations' => 'explanations#show'
+  get 'recommended' => 'recommendeds#show'
   
   resources :camps
   resources :explanations, only: :show
+  resources :recommendeds, only: :show
   resources :users, only: :show
 end
